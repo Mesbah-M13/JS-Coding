62f61ceccc265499213acbf3361c27a947b8c948
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-02 01:56:55 +0600

    new

--- Js Concepts/ques1.js
+++ Js Concepts/ques1.js
@@ -1,13 +1,15 @@
 // Q1: Create a function that returns the last element of an Array
 
-const myArr = [3, 5, 6, 75, 2];
+const myArr = [3, 5, 6, 75, 2,31];
 
 function findLastElement(params) {
-  const lastElem = params[params.length - 1]
-  console.log(lastElem);
+  const lastElem = params[params.length - 1];
+
+  //return myArr.pop(); // it changes the array
+  return lastElem
 }
 
-// function findLast(arr) {
+// function findLastElement(arr) {
 //   let lastElem = [];
 //   for (let i = 0; i <= arr.length - 1; i++) {
 //     if (i == arr.length - 1) {
@@ -17,4 +19,4 @@ function findLastElement(params) {
 //   }
 //   return lastElem;
 // }
-findLastElement(myArr);
+console.log(findLastElement(myArr));
