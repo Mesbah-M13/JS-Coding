5e771043b469f3cad1ea7590394d4219d3d64391
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-04 07:39:41 +0600

    q8

---
+++ Js Concepts/Basic/ques8.js
@@ -0,0 +1,5 @@
+// q8:  Given an array of strings, return a new array where all strings are in uppercase.
+
+const myStrArray = ['hi','bye','khai','nai']
+const newArr = myStrArray.map(arr=> arr.toUpperCase())
+console.log(newArr);
